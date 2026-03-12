0afe3cb86533b62270ee8b7dd7dd42fffd6fa3ab
Jashan <168458181+Jashan122005@users.noreply.github.com>
2026-03-13 01:45:00 +0530

    Refactor blob_service and update image_path length
    Refactor blob_service initialization and adjust image_path column size.

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -8,18 +8,13 @@ from werkzeug.utils import secure_filename
 from flask import flash
 
 blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlockBlobService(
-    account_name=app.config['BLOB_ACCOUNT'],
-    account_key=app.config['BLOB_STORAGE_KEY']
-)
+blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -33,20 +28,17 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-
 class Post(db.Model):
     __tablename__ = 'posts'
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
     author = db.Column(db.String(75))
     body = db.Column(db.String(800))
-    image_path = db.Column(db.String(200))
+    image_path = db.Column(db.String(100))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -60,23 +52,17 @@ class Post(db.Model):
         self.user_id = userId
 
         if file:
-            filename = secure_filename(file.filename)
-            fileextension = filename.rsplit('.', 1)[1]
-            Randomfilename = id_generator()
-            filename = Randomfilename + '.' + fileextension
-
+            filename = secure_filename(file.filename);
+            fileextension = filename.rsplit('.',1)[1];
+            Randomfilename = id_generator();
+            filename = Randomfilename + '.' + fileextension;
             try:
                 blob_service.create_blob_from_stream(blob_container, filename, file)
-
-                if self.image_path:
+                if(self.image_path):
                     blob_service.delete_blob(blob_container, self.image_path)
-
-            except Exception as e:
-                flash(str(e))
-
-            self.image_path = filename
-
+            except Exception:
+                flash(Exception)
+            self.image_path =  filename
         if new:
             db.session.add(self)
-
         db.session.commit()
