340f819fd09315071e25ba8308cf5a5fb0bec9c7
Jashan <168458181+Jashan122005@users.noreply.github.com>
2026-03-13 01:42:31 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,19 +7,26 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-from FlaskWebProject import views
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Configure logging
+# Logging configuration
 logging.basicConfig(level=logging.INFO)
 app.logger.setLevel(logging.INFO)
+app.logger.info("Application startup")
 
 Session(app)
+
 db = SQLAlchemy(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
 
+# Import models and views
+import FlaskWebProject.models
 import FlaskWebProject.views
+
+# Create database tables automatically
+with app.app_context():
+    db.create_all()
