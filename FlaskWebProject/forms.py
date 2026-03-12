77865cc8e9ac134c16f26017d9da808eac09f34f
Jashan <168458181+Jashan122005@users.noreply.github.com>
2026-03-13 01:44:40 +0530

    Update forms.py

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
-
+from wtforms.widgets import TextArea
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -10,16 +10,9 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
     body = TextAreaField('Body', validators=[DataRequired()])
-
-    # Allow jpg and png images
-    image_path = FileField(
-        'Image',
-        validators=[FileAllowed(['jpg', 'png'], 'Only JPG and PNG images are allowed')]
-    )
-
+    image_path = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
     submit = SubmitField('Save')
